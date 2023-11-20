85dca00bf1011281f00a4e60339ef8d038d4dcc4
AGarofalo4544 <89226369+AGarofalo4544@users.noreply.github.com>
2023-11-20 09:53:33 -0500

    Update index.js
    09:53 AM, 11/20/2023 - Alexander Garofalo

--- src/index.js
+++ src/index.js
@@ -42,3 +42,82 @@
 // console.log("Started application on port %d", 10000);
 // });
 
+
+
+// Code deprecated from "/src/InHouse/protoPollution.js"
+// document.getElementById('showJson').addEventListener("click", myDisplay);
+// function myDisplay() {
+//     var x = document.getElementById("output");
+//     if (x.style.display === "none") {
+//         x.style.display = "block";
+//         showList();
+//     } else {
+//         x.style.display = "none";
+//     }
+// }
+// function showList()
+// {
+//     fetch("./users.json")
+//         .then(response => response.json())
+//         .then(jsonData => displayJsonKeysAndValues(jsonData));
+// }
+// function displayJsonKeysAndValues(jsonData)
+// {
+//     const outputElement = document.getElementById('output');
+//     outputElement.innerHTML = ''; // Clear previous content
+//     for (const key in jsonData["users"])
+//     {
+//         const userNums = document.createElement('li');
+//         userNums.textContent = `User ${key}:`;
+//         outputElement.appendChild(userNums);
+//         const list = document.createElement('ul');
+//         userNums.appendChild(list);
+//         for (const key2 in jsonData["users"][key])
+//         {
+//             const listItem = document.createElement('li');
+//             listItem.textContent = `${key2}: ${jsonData["users"][key][key2]}`;
+//             list.appendChild(listItem);
+//         }
+//     }
+// }
+// function createMap()
+// {
+    // return myMap;
+// }
+// function createLiterals()
+// {
+    // let users = {
+    //     "users": 
+    //     [
+    //         {
+    //             "name": "Alex",
+    //             "student": true,
+    //             "admin": false,
+    //             "isPickled": false
+    //         },
+    //         {
+    //             "name": "ADMIN",
+    //             "student": null,
+    //             "admin": true,
+    //             "isPickled": false
+    //         },
+    //         {
+    //             "name": "Cucumber",
+    //             "family": "Cucurbitaceae",
+    //             "genus": "Cucumis",
+    //             "species": "Cucumis sativus",
+    //             "isPickled": false
+    //         },
+    //         {
+    //             "name": "Pickle",
+    //             "family": "Cucurbitaceae",
+    //             "genus": "Cucumis",
+    //             "species": "Cucumis sativus",
+    //             "isPickled": true
+    //         }
+    //     ]
+    // }
+//     return users, myPickles;
+// }
+// createMap();
+// createLiterals();
